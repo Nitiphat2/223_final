cdd0d03ce4db52a0a7df2facff124bcda4e20253
Nitiphat2 <moozknot@gmail.com>
2021-03-29 19:05:19 +0700

    ไม่น่าเชื่อว่าพลาด

--- 6320502410_final_1.c
+++ 6320502410_final_1.c
@@ -3,17 +3,19 @@ int main()
 {
     int n,m;
     scanf("%d%d",&n,&m);
-    int i=0,j,k;
+    int i=0,j,k,v;
     n++;
     while(i!=1)
     {
         j=n;
-        while(j>1)
+        v=0;
+        while(j>=1)
         {
             k=j%10;
             if(k == m)
             {
                 i=1;
+                break;
             }
             j=j/10;
         }

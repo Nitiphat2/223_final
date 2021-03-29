fec2214242856af2f93c18c5146956fdc742dc0b
Nitiphat2 <moozknot@gmail.com>
2021-03-29 17:52:47 +0700

    dddddddddddddddddddddddddddddddd

---
+++ 6320502410_final_3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n[3];
+    int i,j,k,l,m;
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&n[i]);
+        int g[n[i]];
+        for(j=0;j<n[i];j++)
+        {
+            for(k=0;k<n[i];k++)
+            {
+                scanf("%d",&g[i]);
+            }
+        }
+    }
+}

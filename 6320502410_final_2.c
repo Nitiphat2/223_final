d67f76b80d30591ae4fde1f4d5cfdcc047f1f380
Nitiphat2 <moozknot@gmail.com>
2021-03-29 17:31:55 +0700

    กหก

--- 6320502410_final_2.c
+++ 6320502410_final_2.c
@@ -3,10 +3,22 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int i,j,k[n];
+    int i,j,k[n],max=0;
     for(i=0;i<n;i++)
     {
         scanf("%d",&k[i]);
+        if(k[i]>max)
+        {
+            max = k[i];
+        }
     }
-
+    int g[max];
+    for(i=0;i<max;i++)
+    {
+        g[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        g[k[i]-1]=g[k[i]-1]+1;
+    }
 }

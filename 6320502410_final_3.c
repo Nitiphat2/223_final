2f52cdb8b2084eef9b0751f59b0982c7e865cb70
Nitiphat2 <moozknot@gmail.com>
2021-03-29 17:58:20 +0700

    zxzzzxzxzx

--- 6320502410_final_3.c
+++ 6320502410_final_3.c
@@ -11,13 +11,18 @@ int main()
         {
             for(k=0;k<n[i];k++)
             {
-                scanf("%d",&g[i]);
+                scanf("%d",&g[i][k]);
             }
         }
-        int a[n[i]],b[n[i]],c[n[i]];
+        int a[n[i]],b[n[i]],c[2];
         for(j=0;j<n[i];j++)
         {
-
+            for(k=0;k<n[i];k++)
+            {
+               a[j] = a[j] + g[i][k];
+               b[j] = b[j] + g[k][i];
+            }
+            printf("%d %d",a[j],b[j]);
         }
     }
 }

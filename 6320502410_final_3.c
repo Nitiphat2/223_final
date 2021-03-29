8de30f6444ddb83d16e60e0ea255d986da04978e
Nitiphat2 <moozknot@gmail.com>
2021-03-29 17:55:05 +0700

    555

--- 6320502410_final_3.c
+++ 6320502410_final_3.c
@@ -6,13 +6,18 @@ int main()
     for(i=0;i<3;i++)
     {
         scanf("%d",&n[i]);
-        int g[n[i]];
+        int g[n[i]][n[i]];
         for(j=0;j<n[i];j++)
         {
             for(k=0;k<n[i];k++)
             {
                 scanf("%d",&g[i]);
             }
+        }
+        int a[n[i]],b[n[i]],c[n[i]];
+        for(j=0;j<n[i];j++)
+        {
+
         }
     }
 }
